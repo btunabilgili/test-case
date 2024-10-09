72ec580db86730eac6b36d3c8ed34776d5ef10b1
Benhur Tuna Bilgili <btunabilgili@gmail.com>
2024-10-09 20:24:32 +0300

    service user password hashed

--- RiskAnalysis.Application/ServiceUser/ServiceUserProfile.cs
+++ RiskAnalysis.Application/ServiceUser/ServiceUserProfile.cs
@@ -7,7 +7,19 @@ namespace RiskAnalysis.Application
     {
         public ServiceUserProfile()
         {
-            CreateMap<ServiceUserDto, ServiceUser>().ReverseMap();
+            CreateMap<ServiceUserDto, ServiceUser>()
+                .ForMember(dest => dest.Password, opt => opt.MapFrom<CustomResolver>());
+
+            CreateMap<ServiceUser, ServiceUserDto>()
+                .ForMember(dest => dest.Password, opt => opt.Ignore());
+        }
+
+        private class CustomResolver(IPasswordHashService hashService) : IValueResolver<ServiceUserDto, ServiceUser, string>
+        {
+            public string Resolve(ServiceUserDto source, ServiceUser destination, string destMember, ResolutionContext context)
+            {
+                return hashService.HashPassword(source.Password);
+            }
         }
     }
 }
